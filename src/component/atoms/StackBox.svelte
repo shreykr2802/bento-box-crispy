71ccded036dd387c463ad2c0bee2469a0cbe40bf
Shrey Kumar <shreykumar@Shreys-MacBook-Air.local>
2024-06-04 00:08:39 +0530

    Stacks

--- src/component/atoms/StackBox.svelte
+++ src/component/atoms/StackBox.svelte
@@ -1,18 +1,21 @@
 <script>
-    export let image_url;
+    export let stackName;
 </script>
 
-<div class="stack-box" style="background-image: url(/{image_url}.png);"></div>
+<div class="stack-box" style="background-image: url(/images/{stackName}.png);"  {...$$restProps}></div>
 
-<style>
+<style lang="scss">
 	.stack-box {
-		height: 5rem;
-		width: 5rem;
+		height: 4.5rem;
+		width: 4.5rem;
 		background-color: var(--color-stack-box);
-		border-radius: 8px;
-		background-size: 3rem;
+		border-radius: 12px;
+		background-size: 2.5rem;
 		background-repeat: no-repeat;
 		background-position: center;
-        /* background-image: url(/src/lib/images/AWS.png); */
+
+        &:hover {
+            background-color: var(--color-stack-box-hover);
+        }
 	}
 </style>
--- src/component/molecule/HomeContent.svelte
+++ src/component/molecule/HomeContent.svelte
@@ -34,8 +34,60 @@
 			<div class="box32">
 				<section>
 					<h2 class="box-title">Stack I use</h2>
-					<StackBox image_url='AWS'/>
-					<StackBox image_url='CSS3'/>
+					<div class="stack-images">
+					<div class="stacks-used">
+						<StackBox stackName="AWS" />
+						<StackBox stackName="CSS3" />
+						<StackBox stackName="JavaScript" />
+						<StackBox stackName="Express" />
+						<StackBox stackName="Git" />
+						<StackBox stackName="HTML5" />
+						<StackBox stackName="React" />
+						<StackBox stackName="Redux" />
+						<StackBox stackName="CodePen" />
+						<StackBox stackName="Docker" />
+						<StackBox stackName="ESLint" />
+						<StackBox stackName="Figma" />
+						<StackBox stackName="Jest" />
+						<StackBox stackName="Knex.js" />
+						<StackBox stackName="Kubernetes" />
+						<StackBox stackName="Next.js" />
+						<StackBox stackName="Svelte" />
+						<StackBox stackName="Sass" />
+						<StackBox stackName="PostgresSQL" />
+						<StackBox stackName="Terraform" />
+						<StackBox stackName="Vite.js" />
+						<StackBox stackName="TypeScript" />
+						<StackBox stackName="VSCode" />
+						<StackBox stackName="Helm" />
+					</div>
+					<div class="stacks-used" aria-hidden="true">
+						<StackBox stackName="AWS" />
+						<StackBox stackName="CSS3" />
+						<StackBox stackName="JavaScript" />
+						<StackBox stackName="Express" />
+						<StackBox stackName="Git" />
+						<StackBox stackName="HTML5" />
+						<StackBox stackName="React" />
+						<StackBox stackName="Redux" />
+						<StackBox stackName="CodePen" />
+						<StackBox stackName="Docker" />
+						<StackBox stackName="ESLint" />
+						<StackBox stackName="Figma" />
+						<StackBox stackName="Jest" />
+						<StackBox stackName="Knex.js" />
+						<StackBox stackName="Kubernetes" />
+						<StackBox stackName="Next.js" />
+						<StackBox stackName="Svelte" />
+						<StackBox stackName="Sass" />
+						<StackBox stackName="PostgresSQL" />
+						<StackBox stackName="Terraform" />
+						<StackBox stackName="Vite.js" />
+						<StackBox stackName="TypeScript" />
+						<StackBox stackName="VSCode" />
+						<StackBox stackName="Helm" />
+					</div>
+				</div>
 				</section>
 			</div>
 		</div>
@@ -174,6 +226,7 @@
 				.box32 {
 					border: var(--box-border);
 					display: flex;
+					flex-direction: column;
 					border-radius: var(--inner-box-border-radius);
 					height: calc(100% - var(--diff-factor));
 					width: calc(40% - var(--diff-factor));
@@ -185,7 +238,46 @@
 						margin: 1rem;
 						display: flex;
 						flex-direction: column;
-						width: 100%;
+						overflow: hidden;
+						max-width: 90%;
+						position: relative;
+						user-select: none;
+						height: 100%;
+
+						&:hover {
+							.stacks-used {
+								animation-play-state: running;
+							}
+						}
+
+						.stack-images {
+							display: flex;
+							flex-direction: row;
+							position: absolute;
+							align-items: center;
+							height: 100%;
+							gap: 0.5rem;
+						}
+						.stacks-used {
+							flex-shrink: 0;
+							display: flex;
+
+							animation:
+								sidescroll linear infinite 20s,
+								sidescroll linear infinite 40s reverse paused;
+							animation-composition: add;
+							will-change: transform;
+							gap: 0.5rem;
+
+							@keyframes sidescroll {
+								from {
+									transform: translateX(-10%);
+								}
+								to {
+									transform: translateX(calc(-100% + 20%));
+								}
+							}
+						}
 					}
 				}
 			}
