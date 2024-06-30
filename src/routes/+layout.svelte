6d8c03890e82c011930fc3a0334a80451e2d0b69
Shrey Kumar <shreykr2802@gmail.com>
2024-06-30 21:13:30 +0530

    vercel insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,9 @@
 	import Header from './Header.svelte';
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	inject({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 </script>
 
 {#if $page.route.id !== '/'}
